7bb1a548b41101b2b4384b8d2bd38044d9ab2b07
williamlopesdev <wilcypherr@gmail.com>
2024-05-23 19:55:50 -0300

    property buding, event biding e navegação com roteamento

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
+  pensamento = {
+    id : '1',
+    conteudo : 'aprendendo angular',
+    autoria : 'Dev',
+    modelo : 'modelo1'
+
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  criarPensamento(){
+    alert("Novo pensamento Criado")
+  }
+
+  cancelar(){
+    alert("Pensamento Cancelado")
+  }
 }
